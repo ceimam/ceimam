de4de8d608ed7f121b5652795b6b72d4b235257f
deleuzeano <deleuzeano@users.noreply.github.com>
2026-05-28 10:16:06 -0300

    Revert "Remove perfis de Niminon Pinheiro e Vinícius Siqueira do site"
    This reverts commit 08ee1acf70c2acfe7b8c13ba489f2c30e28280c2.

--- extract_pdf.py
+++
@@ -1,6 +0,0 @@
-from pypdf import PdfReader
-reader = PdfReader("C:/Users/guilh/Downloads/Documento sem título (2).pdf")
-for i, p in enumerate(reader.pages):
-    print(f"=== PAGE {i+1} ===")
-    print(p.extract_text())
-    print()
